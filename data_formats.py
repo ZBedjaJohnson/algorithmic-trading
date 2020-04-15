107d6048231ff8a1afc4985ef42b5cd06b28ac04
zbedjajohnson <zbedjajohnson@gmail.com>
2020-04-15 18:09:12 +0100

    iex min class created

---
+++ data_formats.py
@@ -0,0 +1,18 @@
+import backtrader.feeds as btfeeds
+
+class IEXMinuteCSV(btfeeds.GenericCSVData):
+
+  params = (
+    ('nullvalue', 0.0),
+    ('dtformat', ('%Y-%m-%d')),
+    ('tmformat', ('%H:%M')),
+
+    ('datetime', 0),
+    ('time', 1),
+    ('open', 2),
+    ('high', 3),
+    ('low', 4),
+    ('close', 5),
+    ('volume', 6),
+    ('openinterest', -1)
+)
