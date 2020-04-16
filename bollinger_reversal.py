30ecaa748d5bd82f1807aecf4e212c8518e51e01
zbedjajohnson <zbedjajohnson@gmail.com>
2020-04-16 21:12:36 +0100

    stoploss refactor

--- bollinger_reversal.py
+++ bollinger_reversal.py
@@ -7,15 +7,10 @@ class TestStrategy(bt.Strategy):
     def __init__(self):
         self.dataclose = self.datas[0].close
 
-        self.bbands = btind.BollingerBands(period=20,devfactor=2)
+        self.orefs = list()
 
-        self.stoch = btind.Stochastic(period=14,period_dfast=3,period_dslow=3)
-
-        self.order = None
-
-        self.short_signal = bt.And(self.dataclose > self.bbands.top, self.stoch.percK > 80)
-
-        self.long_signal = bt.And(self.dataclose < self.bbands.bot, self.stoch.percK < 20)
+        self.loss_perc = 0.0025
+        self.risk_perc = 0.05
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -23,25 +18,25 @@ class TestStrategy(bt.Strategy):
         
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, Price: {}, Value: {}, Comm: {}'
-                .format(order.executed.price,order.executed.value,order.executed.comm))
+                self.log('BUY EXECUTED, Price: {}, Value: {}, Comm: {}, Type: {}'
+                .format(order.executed.price,order.executed.value,order.executed.comm,order.exectype))
             elif order.issell():
-                self.log('SELL EXECUTED, Price: {}, Value: {}, Comm: {}'
-                .format(order.executed.price,order.executed.value,order.executed.comm))
+                self.log('SELL EXECUTED, Price: {}, Value: {}, Comm: {}, Type: {}'
+                .format(order.executed.price,order.executed.value,order.executed.comm,order.exectype))
 
             self.bar_executed = len(self)    
 
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('Order Canceled/Margin/Rejected')
 
-        self.order = None
+        self.orefs.remove(order.ref)
 
 
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
 
-        self.log('OPERATION PROFIT, GROSS {}, NET {}'.format(trade.pnl, trade.pnlcomm))
+        self.log('OPERATION PROFIT, GROSS {:.2f}, NET {:.2f}, CASH: {:.2f}'.format(trade.pnl, trade.pnlcomm, self.broker.get_cash()))
                     
 
     def log(self, txt):
@@ -50,31 +45,35 @@ class TestStrategy(bt.Strategy):
 
 
     def next(self):
-        if self.order:
+        if self.orefs:
             return
 
         if not self.position:
-            if self.long_signal[0] == True:
-                self.log('BUY CREATE, {}'.format(self.dataclose[0]))
-                self.order = self.buy(size=100)
+            if self.dataclose[0] > self.dataclose[-1]:
+                if self.dataclose[-1] > self.dataclose[-2]:
+                    self.log('BUY CREATE, Price: {}, QTY: {}'.format(self.dataclose[0],self.pos_size()))
+                    mkt_buy = self.buy(size=self.pos_size(), exectype=bt.Order.Market, transmit=False)
+                    stp_sell = self.sell(price=self.stop_price(mkt_buy.isbuy()),size=mkt_buy.size, exectype=bt.Order.Stop, transmit=True,parent=mkt_buy)
+                    self.orefs.extend([mkt_buy.ref,stp_sell.ref])
 
-            elif self.short_signal[0] == True:
-                    self.log('SELL CREATE, {}'.format(self.dataclose[0]))
-                    self.order = self.sell(size=100)
+    def pos_size(self):
+        
+        size = (self.broker.get_cash() * self.risk_perc) / (self.dataclose[0] * self.loss_perc)
 
+        return round(size,0)
+
+    def stop_price(self,dir):
+        if dir == True:
+            stoploss = self.dataclose[0] - self.dataclose[0] * self.loss_perc
         else:
-            if self.position.size > 0 and self.short_signal[0] == True:
-                self.log('SELL CREATE, {}'.format(self.dataclose[0]))
-                self.order = self.sell(size=100)
+            stoploss = self.dataclose[0] + self.dataclose[0] * self.loss_perc
 
-            elif self.position.size < 0 and self.long_signal[0] == True:
-                self.log('BUY CREATE, {}'.format(self.dataclose[0]))
-                self.order = self.buy(size=100)
+        return round(stoploss,2)
 
 def runstrat():
     cerebro = bt.Cerebro()
 
-    datapath = ('./data/AMD-202003-minute.csv')
+    datapath = ('./data/AMD-202002-minute.csv')
 
     data = data_formats.IEXMinuteCSV(dataname=datapath)
 
@@ -83,15 +82,15 @@ def runstrat():
     cerebro.addstrategy(TestStrategy)
 
     cerebro.broker.setcash(10000)
-    cerebro.broker.setcommission(commission=0.0001)
+    cerebro.broker.setcommission(commission=0,leverage=20)
     
-    print('Starting Portfolio Value: {}'.format(cerebro.broker.getvalue()))
+    print('Starting Portfolio Value: {:.2f}'.format(cerebro.broker.getvalue()))
 
     cerebro.run()
 
-    print('Final Portfolio Value: {}'.format(cerebro.broker.getvalue()))
+    print('Final Portfolio Value: {:.2f}'.format(cerebro.broker.getvalue()))
 
-    cerebro.plot()
+    # cerebro.plot()
 
 
 if __name__ == '__main__':
--- stop_loss_test.py
+++ stop_loss_test.py
@@ -7,8 +7,10 @@ class TestStrategy(bt.Strategy):
     def __init__(self):
         self.dataclose = self.datas[0].close
 
-        self.order = None
+        self.orefs = list()
 
+        self.loss_perc = 0.0025
+        self.risk_perc = 0.05
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -27,14 +29,14 @@ class TestStrategy(bt.Strategy):
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('Order Canceled/Margin/Rejected')
 
-        self.order = None
+        self.orefs.remove(order.ref)
 
 
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
 
-        self.log('OPERATION PROFIT, GROSS {:.2f}, NET {:.2f}'.format(trade.pnl, trade.pnlcomm))
+        self.log('OPERATION PROFIT, GROSS {:.2f}, NET {:.2f}, CASH: {:.2f}'.format(trade.pnl, trade.pnlcomm, self.broker.get_cash()))
                     
 
     def log(self, txt):
@@ -43,19 +45,33 @@ class TestStrategy(bt.Strategy):
 
 
     def next(self):
-        if self.order:
+        if self.orefs:
             return
 
         if not self.position:
             if self.dataclose[0] > self.dataclose[-1]:
                 if self.dataclose[-1] > self.dataclose[-2]:
-                    self.log('BUY CREATE, {}'.format(self.dataclose[0]))
-                    self.order = self.buy(size=100)
+                    self.log('BUY CREATE, Price: {}, QTY: {}'.format(self.dataclose[0],self.pos_size()))
+                    mkt_buy = self.buy(size=self.pos_size(), exectype=bt.Order.Market, transmit=False)
+                    stp_sell = self.sell(price=self.stop_price(mkt_buy.isbuy()),size=mkt_buy.size, exectype=bt.Order.Stop, transmit=True,parent=mkt_buy)
+                    self.orefs.extend([mkt_buy.ref,stp_sell.ref])
 
 
-# mainside = self.buy(price=13.50, exectype=bt.Order.Limit, transmit=False)
-# lowside  = self.sell(price=13.00, size=mainside.size, exectype=bt.Order.Stop,
-#                      transmit=False, parent=mainside)
+    def pos_size(self):
+        
+        size = (self.broker.get_cash() * self.risk_perc) / (self.dataclose[0] * self.loss_perc)
+
+        return round(size,0)
+
+
+    def stop_price(self,dir):
+        if dir == True:
+            stoploss = self.dataclose[0] - self.dataclose[0] * self.loss_perc
+        else:
+            stoploss = self.dataclose[0] + self.dataclose[0] * self.loss_perc
+
+        return round(stoploss,2)
+
 
 def runstrat():
     cerebro = bt.Cerebro()
@@ -69,7 +85,7 @@ def runstrat():
     cerebro.addstrategy(TestStrategy)
 
     cerebro.broker.setcash(10000)
-    cerebro.broker.setcommission(commission=0.0001)
+    cerebro.broker.setcommission(commission=0,leverage=20)
     
     print('Starting Portfolio Value: {:.2f}'.format(cerebro.broker.getvalue()))
 
@@ -77,7 +93,7 @@ def runstrat():
 
     print('Final Portfolio Value: {:.2f}'.format(cerebro.broker.getvalue()))
 
-    cerebro.plot()
+    # cerebro.plot()
 
 
 if __name__ == '__main__':
